271fcbe031298574d594e63c276fe136e206efd6
SMinhChau <minhchaunguyenmtp@gmail.com>
2025-03-16 15:13:13 +0700

    hidden box Experiences

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,36 +2,36 @@ import HomeContent from '@/component/home-content';
 import './home.css';
 import { ContactData, DetailInfo, SkillsData } from './data';
 import ContactForm from '@/component/common/form/contact';
-import { MyExperiences } from '@/utils/content';
-import CardContent from '@/component/card-content';
+// import { MyExperiences } from '@/utils/content';
+// import CardContent from '@/component/card-content';
 
 import Images from '/images/home/avatar.jpg';
 
 const HomePage = () => {
-  const Experiences = () => {
-    return (
-      <HomeContent id="experiences">
-        <h2 className="title">
-          My
-          <span className="sub-title"> Experiences</span>
-        </h2>
+  // const Experiences = () => {
+  //   return (
+  //     <HomeContent id="experiences">
+  //       <h2 className="title">
+  //         My
+  //         <span className="sub-title"> Experiences</span>
+  //       </h2>
 
-        <p className="exps-description">
-          There are many variations of passages ofLorem Ipsum available, but the I’ve written a few
-          thousand words on why traditional “semantic class names” are the reason CSS is hard to
-          maintain, but the truth is never going to believe me until you actually try it. If you can
-          suppress the urge to retch long enough to give it a chance, I really think you’ll wonder
-          how you ever worked with CSS any other way.
-        </p>
+  //       <p className="exps-description">
+  //         There are many variations of passages ofLorem Ipsum available, but the I’ve written a few
+  //         thousand words on why traditional “semantic class names” are the reason CSS is hard to
+  //         maintain, but the truth is never going to believe me until you actually try it. If you can
+  //         suppress the urge to retch long enough to give it a chance, I really think you’ll wonder
+  //         how you ever worked with CSS any other way.
+  //       </p>
 
-        <div className="cards">
-          {MyExperiences.map((item, index) => {
-            return <CardContent key={item.id || index} item={item} />;
-          })}
-        </div>
-      </HomeContent>
-    );
-  };
+  //       <div className="cards">
+  //         {MyExperiences.map((item, index) => {
+  //           return <CardContent key={item.id || index} item={item} />;
+  //         })}
+  //       </div>
+  //     </HomeContent>
+  //   );
+  // };
 
   const Information = () => {
     return (
